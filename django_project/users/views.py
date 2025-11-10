dee4360e318e3454d3091b2aa6c27033ab4f04d4
Al-ameen Abiola <alameenabiola3@gmail.com>
2025-11-11 00:27:48 +0100

    created user app, used flashmessages, csrf token and redirect module

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/registr.html', {'form': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account creared for {username}!')
+            return redirect('realty-home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
