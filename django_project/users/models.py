0f9eb50a10be0d0aa1bc59b2037193092c60b9eb
Al-ameen Abiola <alameenabiola3@gmail.com>
2025-11-26 13:17:16 +0100

    resized the profile image automatically

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+'''import the pillow library to work with images'''
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +10,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()  # Call the real save() method
+
+        ''' Open image using pillow 
+            resieze if necessary
+        '''
+        img = Image.open(self.image.path)
+
+        # checking if current image is larger than 300px 
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)  # resize image
+            img.save(self.image.path)  # save it back to the path
