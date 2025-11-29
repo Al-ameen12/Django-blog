0cff3b6af40e895c290e67678153df7a4eb789cb
Al-ameen Abiola <alameenabiola3@gmail.com>
2025-11-29 12:01:37 +0100

    class based views - used List Views to list post on the blog home page

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,14 @@
+'''
+    imported PostListView to be used for the home page
+    updated the path for the home page to use PostListView.as_view()
+
+    
+'''
 from django.urls import path
+from .views import PostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='realty-home'),
+    path('',PostListView.as_view(), name='realty-home'),
     path('about/', views.about, name='realty-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,15 @@
+'''Using Class Based Views (CBVs)
+    import list view from django.views.generic
+    create a class named PostList that inherits from ListView
+    set the model attribute to Post
+    set the template_name attribute to 'blog/home.html'
+
+    changed the template to be used by view to 'blog/home.html'
+    set the context_object_name attribute to 'posts' to change the default name of the list object
+    order the posts by date_posted in descending order
+'''
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 def home(request):
@@ -7,6 +18,12 @@ def home(request):
         'title' : 'home'
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post #it tells the ListView what model to query in other to create the list
+    template_name = 'blog/home.html' #specifies the template to be used  <app>/<model>_<viewtype>.html
+    context_object_name = 'posts' #this is used to change the default name of the list object from object_list to posts
+    ordering = ['-date_posted'] #orders the posts by date_posted in descending order
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
